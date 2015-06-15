311decb4a435c503bd32a3ea3b5271c9bd1e2092
Philipp Meisberger <team@pm-codeworks.de>
2015-06-15 15:41:11 +0200

    Added some options; Changed licence to D-FSL

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
     name            = 'ddns-updater',
-    version         = '1.0',
+    version         = '1.1',
     description     = 'DDNS client to register your dynamic IP address.',
     author          = 'Philipp Meisberger',
     author_email    = 'team@pm-codeworks.de',
     url             = 'http://www.pm-codeworks.de/ddns-updater.html',
-    license         = 'BSD 3.0 License',
+    license         = 'D-FSL',
     package_dir     = {'': 'files'},
 )
