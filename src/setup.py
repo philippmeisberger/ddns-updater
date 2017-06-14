b092b1c96baf6f629db820ded6f187e56869f27a
Philipp Meisberger <team@pm-codeworks.de>
2017-06-14 20:46:18 +0200

    Increased version number to 1.2; Updated changelog

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name            = 'ddns-updater',
-    version         = '1.1',
+    version         = '1.2',
     description     = 'DDNS client to update your dynamic IP address',
     author          = 'Philipp Meisberger',
     author_email    = 'team@pm-codeworks.de',
